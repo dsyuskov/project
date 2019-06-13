81a86115db8025c27d658f56988561113ba3d885
Int_21h <yuskovss@gmail.com>
2019-06-13 16:26:46 +0400

    Begin ghostDropdown

--- src/components/dropdown/ghost-dropdown/ghost-dropdown.js
+++ src/components/dropdown/ghost-dropdown/ghost-dropdown.js
@@ -1,7 +1,8 @@
 //https://habr.com/ru/post/38208/
 $(document).ready(function(){
-    $(".dropdown").click(function(){
-        $("#panel").slideToggle("slow");
+    $(".ghostDropdown").click(function(){
+        $("#ghostDropdownList").slideToggle("slow");
         $(this).toggleClass("active");
+        $("#ghostDropdownList").toggleClass("open");
     });
 });
