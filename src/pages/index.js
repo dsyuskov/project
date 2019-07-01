15c4390610144af4fd03dbfc0b4617d9426c0446
Int_21h <yuskovss@gmail.com>
2019-07-01 08:30:54 +0400

    add rich-checkbox-buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-//require('../../node_modules/jquery-ui/ui/jquery-1-7.js');
 import './index.scss';
 import '../components/fields/dropdown/date-dropdown/datepicker.js';
 import '../components/fields/dropdown/ghost-dropdown/ghost-dropdown.js';
